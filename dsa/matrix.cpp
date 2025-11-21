c3f91a519f7f1899ff36b2a1cb2df9e1299c6100
Shashi Bhushan Rai <shashi.rai@bhojpur-consulting.org>
2025-11-21 13:26:27 +0530

    Matrix

---
+++ dsa/matrix.cpp
@@ -0,0 +1,42 @@
+
+// Copyright (c) 2018 Bhojpur Consulting Private Limited, India. All rights reserved.
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+#include "matrix.h"
+
+Matrix::Matrix(int rows, int cols) : data(rows, std::vector<double>(cols, 0)) {
+
+}
+
+void Matrix::set(int row, int col, double value) {
+    data[row][col] = value;
+}
+
+double Matrix::get(int row, int col) const {
+    return data[row][col];
+}
+
+int Matrix::getRows() const {
+    return data.size();
+}
+
+int Matrix::getCols() const {
+    return data[0].size();
+}
